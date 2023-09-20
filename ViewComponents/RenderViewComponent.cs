07a8dc786d85ebf9edee734a98c16a75c1ea4de4
Chung30 <lhongquangtrung@gmail.com>
2023-09-20 15:13:46 +0700

    lab2

---
+++ ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1.ViewComponents
+{
+    public class RenderViewComponent : ViewComponent
+    {
+        private List<MenuItem> MenuItems = new List<MenuItem>();
+
+        public RenderViewComponent() {
+            MenuItems.Add(new MenuItem(1, "Branches", "Branches/List"));
+            MenuItems.Add(new MenuItem(2, "Students", "Students/List"));
+            MenuItems.Add(new MenuItem(3, "Subjects", "Subjects/List"));
+            MenuItems.Add(new MenuItem(4, "Courses", "Courses/List"));
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderLeftMenu", MenuItems);
+        }
+    }
+}
